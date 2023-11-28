a144ba79a4d204e21e458ab09ba92ff3ae71aa02
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2023-11-29 01:33:05 +0900

    save

--- sample_processInteractionByUserRespectively.js
+++ sample_processInteractionByUserRespectively.js
@@ -1,125 +1,172 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { handleInteraction, processAllUserInteractions } = require('./interactionHandler');
-
-const userInteractions = new Map();
+const { Client, GatewayIntentBits, ModalBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+const { event1, event2, event3 } = require('./events.js')
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
   ],
 });
+
+const EventType = {
+  INTERACTION: 1,
+  API: 2,
+};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('interactionCreate', (interaction) => {
-  handleInteraction(interaction);
+client.on('messageCreate', async (message) => {
+	/*
+	if (message.content === '!start_create_gamble') {
+		const buttons = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setLabel('start creating gamble')
+					.setCustomId('startCreatingGamble')
+					.setDisabled(false)
+					.setStyle(ButtonStyle.Primary),
+			)
+
+	await message.channel.send({
+		content: '',
+		components: [buttons],
+		ephemeral: false
+	})
+	  }
+	  */
+})
+
+client.on('interactionCreate', async (interaction) => {
+	handleEvent(interaction);
 });
 
-function setInstructions(series) {
-  const struct_ = {
+const events = new Map();
 
-  }
-  return struct_;
+class eventInteraction {
+	constructor(event) {
+	  this.type = EventType.INTERACTION;
+	  this.eventId = event.customId;
+	  this.expectedDataType = EventType.INTERACTION;
+	  this.action = async (interaction) => {
+		await event.action(interaction);
+	  };
+	  this.response = async (interaction) => {
+		await event.response(interaction);
+	  };
+	  this.if_allowBack = false;
+	}
+}
+  
+class eventAPI {
+	constructor(event) {
+		this.type = EventType.API;
+		this.eventId = event.id;
+		this.expectedDataType = undefined;
+		this.action = event.action;
+		this.response = event.response;
+		this.if_allowBack = false;
+	}
 }
 
-processInteractions();
+class processEvent {
+	constructor(userId, seriesInstructions) {
+	  this.cur_step = 0;
+	  this.userId = userId;
+	  this.aipId = 0; // tmp
+	  this.series = seriesInstructions;//you gatta arrange properties uniformly
+	  this.triggeredEvent = this.series[Object.keys(seriesInstructions)[0]];
+	  this.triggeredEventId = this.triggeredEvent.eventId;
+	}
 
-enum {
-  INTERACTION = 1,
-  API,
-}
-
-const createGamble() {
-    const 
-}
-
-const sendModal() {
-
-}
-
-class presetInteraction(action) {
-    construct() {
-      type = INTERACTION,
-      customId = action.ui.customId,
-      expectedDataType = INTERACTION,
-      act = action,
-      if_allowBack = false;
-    }
-}
-
-class presetAPI(action) {
-    construct() {
-      type = API,
-      id = ,
-      expectedDataType = ,
-      act = action,
-      if_allowBack = false,
-    }
-}
-
-class processEvent(userId, seriesInstructions) {
-  construct() {
-    cur_step = 0,
-    userId = userId,
-    aipId = 0;//tmp
-    series = setInstructions(seriesInstructions),//series
-    
-  }
-  progressOne(param) {
-
-  }
-  if_expectingResult() {
-
-  }
-  displayCurStatus() {
-
-  }
-  timeout() {
-
-  }
-}
-
-const commandCreateGamble = presetInteraction(createGamble);
-const commandSendModal = presetInteraction(sendModal);
-
-
-const event = processEvent(id, {commandCreateGamble, commandSendModal, e3});
-
-
-
-
-function handleInteraction(interaction) {
-  const buttonActions = {
-    'selection0': { action: addInteraction, reply: firstSelection },
-    'selection1': { action: addInteraction, reply: secondSelection },
-    'selection2': { action: addInteraction, reply: thirdSelection },
-    'selection3': { action: addInteraction, reply: processFourInteractions },
-    'id4': { action: handleId4 },
-  };
-
-  const buttonAction = buttonActions[interaction.buttonCustomId];
-
-  if (buttonAction) {
-    buttonAction.action(interaction);
-    if (buttonAction.reply) {
-      interaction.reply(buttonAction.reply);
-    }
-  }
-}
-
-function addInteraction(interaction) {
-  const userId = interaction.user.id;
-
-  if (!userInteractions.has(userId)) {
-    userInteractions.set(userId, []);
+  proceedToNext(param) {//->command
+	  if (!this.if_expectingResult(param)) {
+		  return;
+	  }
+	  const waitingEvent = this.getWaitingEvent();
+	  this.execute_response(waitingEvent);
+	  
+	  this.cur_step++;
+	  const nextWaitingEvent = this.getWaitingEvent();
+	  this.execute_act(nextWaitingEvent);
   }
 
-  userInteractions.get(userId).push(interaction);
+  execute_response(event) {
+	event.response;
+  }
+
+  execute_act(event) {
+	event.action;
+  }
+
+  if_expectingResult(param) {
+	  if (this.series[this.cur_step].expectedDataType === INTERACTION) {
+		  if (if_interaction(param)) {//if param is interaction, it has message
+			  return false;
+		  }
+	  }
+	  return true;
+  }
+
+  getWaitingEvent() {
+	return this.series[Object.keys(seriesInstructions)[this.cur_step]];
+  }
+/*
+  displayCurStatus() {}
+  timeout() {}
+  */
 }
 
+function if_interaction(param) {
+  return param.message;
+}
 
-client.login('YOUR_BOT_TOKEN');
+async function handleEvent(param) {
+	let expectedUserId = undefined;
+	let expectedEventId = undefined;
+	
+	if (if_interaction(param)) {
+	  expectedUserId = param.user.id;
+	  expectedEventId = param.customId;
+	}
+	// add branch when event occurred by others
+	addEvent(expectedUserId, expectedEventId);
+	console.log(events);
+	for (const [userId, event] of events.entries()) {
+		console.log(userId, expectedUserId, expectedEventId);
+		if (userId === expectedUserId && event.waitingEvent && event.getWaitingEvent().eventId === expectedEventId) {
+			console.log(true);
+			userEvent.proceedToNext(param);
+	  	}
+		console.log(false);
+	}
+  }
+  
+  function addEvent(expectedUserId, expectedEventId) {
+	if (!events.has(expectedUserId)) {
+	  events.set(expectedUserId, []);
+	}
+  
+	for (const event of instances_events) {
+	  if (event.triggeredEventId === expectedEventId) {
+		const cloneEvent = Object.assign(Object.create(Object.getPrototypeOf(event)), event);
+		cloneEvent.userId = expectedUserId;
+		events.get(expectedUserId).push(cloneEvent);
+		break;
+	  }
+	}
+	
+  }
+const interactoinEvent1 = new event1();
+const interactoinEvent2 = new event2();
+const interactoinEvent3 = new event3();
+
+const commandInteractoinEvent1 = new eventInteraction(interactoinEvent1);
+const commandInteractoinEvent2 = new eventInteraction(interactoinEvent2);
+const commandInteractoinEvent3 = new eventInteraction(interactoinEvent3);
+
+//const event = processEvent(id, {commandCreateGamble, commandSendGambleModal});
+instances_events = [
+  new processEvent(undefined, { commandInteractoinEvent1, commandInteractoinEvent2, commandInteractoinEvent3 })]
